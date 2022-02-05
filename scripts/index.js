acb536c4b5be1bad0a6992e75919f2d1446b857a
Maria Nikolaidou <marnicolaidou@gmail.com>
2022-02-05 16:32:24 +0100

    change logo + favicons, remove overlay, set date automatically

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,4 @@
 window.onload = function(){
-    const overlay = document.querySelector('.overlay');
-    overlay.classList.add('overlay-down');
-    setTimeout(function(){
-        overlay.classList.remove('overlay-down');
-        overlay.classList.add('overlay-up');
-        
-    },900)
+    const currentYear = new Date().getFullYear();
+    document.querySelector('.currentYear').innerHTML = currentYear;
 }
